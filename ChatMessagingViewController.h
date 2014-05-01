2310a969aacfc24dfb5c819566f4f27ec2880e63
pawanmit <mitpawan@gmail.com>
2014-05-01 13:49:15 -0700

    adding skeleton view controller for chat request

---
+++ ChatMessagingViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatMessagingViewController.h
+//  uiosv2
+//
+//  Created by Mittal, Pawan on 5/1/14.
+//  Copyright (c) 2014 umanly. All rights reserved.
+//
+
+#import "UmanlyViewController.h"
+
+@interface ChatMessagingViewController : UmanlyViewController
+
+@end
---
+++ uiosv2/ChatMessagingViewController.m
@@ -0,0 +1,38 @@
+//
+//  ChatMessagingViewController.m
+//  uiosv2
+//
+//  Created by Mittal, Pawan on 5/1/14.
+//  Copyright (c) 2014 umanly. All rights reserved.
+//
+
+#import "ChatMessagingViewController.h"
+
+@interface ChatMessagingViewController ()
+
+@end
+
+@implementation ChatMessagingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
